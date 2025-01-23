3f25a4d4d1c7a9a3fb029f1f85a4ed07749d7f1a
BewAthithan <biw.a.srichumpa@gmail.com>
2025-01-23 11:07:25 +0700

    set w/ CronExpression

--- src/alert-cron/alert-cron.service.ts
+++ src/alert-cron/alert-cron.service.ts
@@ -15,30 +15,31 @@ export class AlertCronService {
   //     // Add your task logic here
   //   }
 
-  // Custom Cron Expression: Runs every minute
-  @Cron('*/1 * * * *')
-  async handleMinuteTask() {
-    console.log('Task is running every minute!');
-    const payload = { message: 'Test task is running every minute!' };
+  // // Custom Cron Expression: Runs every minute
+  // @Cron('*/1 * * * *')
+  // async handleMinuteTask() {
+  //   console.log('Task is running every minute!');
+  //   const payload = { message: 'Test task is running every minute!' };
 
-    try {
-      // Sending the API request
-      const response = await lastValueFrom(
-        this.httpService.post(this.apiUrl, payload, {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: `Bearer ${this.token}`,
-          },
-        }), // Use `.get` for GET requests
-      );
-      console.log('API Response:', response.data);
-    } catch (error) {
-      console.error('Error triggering API:', error.message);
-    }
-  }
+  //   try {
+  //     // Sending the API request
+  //     const response = await lastValueFrom(
+  //       this.httpService.post(this.apiUrl, payload, {
+  //         headers: {
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           Authorization: `Bearer ${this.token}`,
+  //         },
+  //       }), // Use `.get` for GET requests
+  //     );
+  //     console.log('API Response:', response.data);
+  //   } catch (error) {
+  //     console.error('Error triggering API:', error.message);
+  //   }
+  // }
 
   // Custom Task with Dynamic Logging
-  @Cron('0 9 * * *') // Runs daily at 9:00
+  // @Cron('0 9 * * *') // Runs daily at 9:00
+  @Cron(CronExpression.EVERY_DAY_AT_9AM) // Runs daily at 9:00
   async handle9AMTask() {
     console.log('Custom task is running at 9:00!');
     const payload = { message: 'น้องมิวกินน้ำตอนเช้ารึยัง' };
@@ -60,7 +61,8 @@ export class AlertCronService {
   }
 
   // Custom Task with Dynamic Logging
-  @Cron('0 14 * * *') // Runs daily at 14:00
+  // @Cron('0 14 * * *') // Runs daily at 14:00
+  @Cron(CronExpression.EVERY_DAY_AT_2PM) // Runs daily at 14:00
   async handle2PMTask() {
     console.log('Custom task is running at 14:00!');
     const payload = { message: 'น้องมิวกินน้ำตอนเที่ยงรึยัง' };
@@ -82,7 +84,8 @@ export class AlertCronService {
   }
 
   // Custom Task with Dynamic Logging
-  @Cron('0 20 * * *') // Runs daily at 20:00
+  // @Cron('0 20 * * *') // Runs daily at 20:00
+  @Cron(CronExpression.EVERY_DAY_AT_8PM) // Runs daily at 20:00
   async handle8PMTask() {
     console.log('Custom task is running at 20:00!');
     const payload = { message: 'น้องมิวกินน้ำตอนเย็นรึยัง' };
